5ee81f086c3710fea969010c55a3c4d6749ae2ca
Leonardo Gomes <leonardo.delfica@gmail.com>
2018-08-17 01:57:12 -0300

    Returning address by param name in API

--- app/controllers/api/v1/geolocation_controller.rb
+++ app/controllers/api/v1/geolocation_controller.rb
@@ -2,10 +2,15 @@ module Api
   module V1
     class GeolocationController < ApplicationController
       def index
-        @a = Geocoder.search("Paris")
+        @a = Geocoder.search(geolocation_params)
         
-        render json: @a.first.data
+        render json: @a.first.coordinates, status: :ok
       end
+
+      def geolocation_params
+        params.require(:name)
+      end
+
     end
   end
 end
