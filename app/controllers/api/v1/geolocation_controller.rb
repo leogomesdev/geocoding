a946086722f76ac66404524f988bb44fdd885edd
Leonardo Gomes <leonardo.delfica@gmail.com>
2018-08-17 02:36:44 -0300

    Refactoring to use a class for process data and rules

--- app/controllers/api/v1/geolocation_controller.rb
+++ app/controllers/api/v1/geolocation_controller.rb
@@ -1,14 +1,26 @@
 module Api
   module V1
     class GeolocationController < ApplicationController
+      rescue_from ActionController::ParameterMissing, :with => :parameter_not_found
+
       def index
-        @a = Geocoder.search(geolocation_params)
-        
-        render json: @a.first.coordinates, status: :ok
+        @geocoder = GeocoderResolver.new query_name: geocoder_params
+
+        if @geocoder.get_coordinates.nil?
+          head :not_found
+        else
+          render json: @geocoder.get_coordinates, status: :ok
+        end
       end
 
-      def geolocation_params
-        params.require(:name)
+      private
+
+      def geocoder_params
+        params.require(:geocoder_resolver).require(:query_name)
+      end
+
+      def parameter_not_found(error)
+        render :json => {:error => error.message}, :status => :not_found
       end
 
     end
